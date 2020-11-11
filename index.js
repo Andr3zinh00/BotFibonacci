7ce20069d8b850892dbb634ce6536ab3da5b5173
André Luiz Dias de Oliveira <andrelp1015@gmail.com>
2020-11-11 19:13:18 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const config = require('./config');
+const Twit = require('twit');
+const bot = new Twit(config);
+
+(function () {
+
+  let a = 0;
+  let b = 1;
+  let aux;
+  function fib() {
+    aux = b;
+    if (a === Infinity) {
+      twetta(`É isso, o Javascript só me permite escrever números até: ` + aux);
+      process.exit();
+    }
+    twetta(a);
+    b = a + b;
+    a = aux;
+    setTimeout(fib, 18000000);
+  }
+  fib();
+
+  function twetta(status) {
+    bot.post('statuses/update', { status })
+  }
+})();  
