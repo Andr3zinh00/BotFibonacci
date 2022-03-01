801c348f743425de0e64bea8bc2d26da2935e83f
andr30z <andrelp1015@gmail.com>
2022-02-28 22:05:25 -0300

    reset bot

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ const bot = new Twit(config);
 
 const map: { [x: number]: boolean } = {};
 (function () {
-  let a = 5.935672630284151e+118;
-  let b = 9.604120061892246e+118;
+  let a = 6.380822626578357e+128;
+  let b = 1.032438788598816e+129;
   let aux;
   function fib() {
     aux = b;
