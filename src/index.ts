ce999082c251a024acf34f2659f8cc2ca2c5802f
andr30z <andrelp1015@gmail.com>
2022-03-11 21:28:18 -0300

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ const bot = new Twit(config);
 
 const map: { [x: number]: boolean } = {};
 (function () {
-  let a = 1.137969253983602e+209;
-  let b = 1.8412729310978296e+209;
+  let a = 7.560484372722881e+218;
+  let b = 1.223312068647805e+219;
   let aux;
   function fib() {
     aux = b;
